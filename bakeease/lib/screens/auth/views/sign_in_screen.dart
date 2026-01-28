5a8908603b79432175d24206d6aaf06ec2a6a7d3
Prasad Manamperi <prasadmanamperi130@gmail.com>
2026-01-28 22:18:07 +0530

    create sign in screen

--- bakeease/lib/screens/auth/views/sign_in_screen.dart
+++ bakeease/lib/screens/auth/views/sign_in_screen.dart
@@ -10,6 +10,28 @@ class SignInScreen extends StatefulWidget {
 class _SignInScreenState extends State<SignInScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: Column(
+        children: [
+          const SizedBox(height: 20),
+          TextField(
+            decoration: InputDecoration(
+              hintText: 'Email',
+              border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
+            ),
+          ),
+          const SizedBox(height: 10),
+          TextField(
+            obscureText: true,
+            decoration: InputDecoration(
+              hintText: 'Password',
+              border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
+            ),
+          ),
+          // Add your Login Button here...
+        ],
+      ),
+      );
   }
 }
