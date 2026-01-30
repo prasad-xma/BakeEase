eb5246690eca13fa519861719227a0b61302dc17
Prasad Manamperi <prasadmanamperi130@gmail.com>
2026-01-30 19:55:01 +0530

    create sign up screen with form validation and state management

--- bakeease/lib/screens/auth/views/sign_up_screen.dart
+++ bakeease/lib/screens/auth/views/sign_up_screen.dart
@@ -1,4 +1,9 @@
+import 'package:bakeease/components/my_text_field.dart';
+import 'package:bakeease/screens/auth/blocs/sign_up_bloc/sign_up_bloc.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+// import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class SignUpScreen extends StatefulWidget {
   const SignUpScreen({super.key});
@@ -8,8 +13,162 @@ class SignUpScreen extends StatefulWidget {
 }
 
 class _SignUpScreenState extends State<SignUpScreen> {
+  final passwordController = TextEditingController();
+  final emailController = TextEditingController();
+  final nameController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+
+  IconData iconPassword = CupertinoIcons.eye_fill;
+
+  bool obscurePassword = true;
+  bool signUpRequired = false;
+
+  bool containsUpperCase = false;
+  bool containsLowerCase = false;
+  bool containsNumber = false;
+  bool containsSpecialChar = false;
+  bool contains8Length = false;
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return BlocListener<SignUpBloc, SignUpState>(
+      listener: (context, state) {
+        if (state is SignUpSuccess) {
+          setState(() {
+            signUpRequired = false;
+          });
+        } else if (state is SignUpProcess) {
+          setState(() {
+            signUpRequired = true;
+          });
+        } else if (state is SignUpFailure) {
+          // setState(() {
+          //   signUpRequired = false;
+          // });
+          return;
+        }
+      },
+      child: Form(
+        key: _formKey,
+        child: Center(
+          child: Column(
+            children: [
+              const SizedBox(height: 20),
+              SizedBox(
+                width: MediaQuery.of(context).size.width * 0.9,
+                child: MyTextField(
+                  controller: emailController,
+                  hintText: 'Email',
+                  obscureText: false,
+                  keyboardType: TextInputType.emailAddress,
+                  prefixIcon: const Icon(CupertinoIcons.mail_solid),
+                  validator: (val) {
+                    if (val!.isEmpty) {
+                      return 'Please fill in this field';
+                    } else if (!RegExp(
+                      r'^[\w-\.]+@([\w-]+.)+[\w-]{2,4}$',
+                    ).hasMatch(val)) {
+                      return 'Please enter a valid email';
+                    }
+                    return null;
+                  },
+                ),
+              ),
+              const SizedBox(height: 10),
+              SizedBox(
+                width: MediaQuery.of(context).size.width * 0.9,
+                child: MyTextField(
+                  controller: passwordController,
+                  hintText: 'Password',
+                  obscureText: obscurePassword,
+                  keyboardType: TextInputType.visiblePassword,
+                  prefixIcon: const Icon(CupertinoIcons.lock_fill),
+                  onChanged: (val) {
+                    if (val!.contains(RegExp(r'[A-Z]'))) {
+                      setState(() {
+                        containsUpperCase = true;
+                      });
+                    } else {
+                      setState(() {
+                        containsUpperCase = false;
+                      });
+                    }
+                    if (val.contains(RegExp(r'[a-z]'))) {
+                      setState(() {
+                        containsLowerCase = true;
+                      });
+                    } else {
+                      setState(() {
+                        containsLowerCase = false;
+                      });
+                    }
+                    if (val.contains(RegExp(r'[0-9]'))) {
+                      setState(() {
+                        containsNumber = true;
+                      });
+                    } else {
+                      setState(() {
+                        containsNumber = false;
+                      });
+                    }
+                    if (val.contains(
+                      RegExp(r'^(?=.*?[!@#$&*~`)\%\-(_+=;:,.<>/?"[{\]}\|^])'),
+                    )) {
+                      setState(() {
+                        containsSpecialChar = true;
+                      });
+                    } else {
+                      setState(() {
+                        containsSpecialChar = false;
+                      });
+                    }
+                    if (val.length >= 8) {
+                      setState(() {
+                        contains8Length = true;
+                      });
+                    } else {
+                      setState(() {
+                        contains8Length = false;
+                      });
+                    }
+                    return null;
+                  },
+                  suffixIcon: IconButton(
+                    onPressed: () {
+                      setState(() {
+                        obscurePassword = !obscurePassword;
+
+                        if (obscurePassword) {
+                          iconPassword = CupertinoIcons.eye_fill;
+                        
+                        } else {
+                          iconPassword = CupertinoIcons.eye_slash_fill;
+                        }
+                      });
+                    },
+                    icon: Icon(iconPassword),
+                  ),
+                  validator: (val) {
+                    if (val!.isEmpty) {
+                      return 'Please fill in this field';
+                    } else if (!RegExp(r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[!@#\$&*~`)\%\-(_+=;:,.<>/?"[{\]}\|^]).{8,}$').hasMatch(val)) {
+                      return 'Please enter a valid password';
+                    }
+                    return null;
+                  },
+                ),
+              ),
+              const SizedBox(height: 10,),
+              Row(
+                
+
+
+
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
