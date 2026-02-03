6024ac637df56d4d5960c9b64e986afcdc7ab68c
Prasad Manamperi <prasadmanamperi130@gmail.com>
2026-02-04 03:22:00 +0530

    update: wrap with the SignInBloc provider

--- bakeease/lib/app_view.dart
+++ bakeease/lib/app_view.dart
@@ -1,4 +1,5 @@
 import 'package:bakeease/blocs/authentication_bloc/authentication_bloc.dart';
+import 'package:bakeease/screens/auth/blocs/sign_in_bloc/sign_in_bloc.dart';
 import 'package:bakeease/screens/auth/views/welcome_screen.dart';
 import 'package:bakeease/screens/home/views/home_screen.dart';
 import 'package:flutter/material.dart';
@@ -17,19 +18,23 @@ class MyAppView extends StatelessWidget {
           surface: Colors.grey.shade100,
           onSurface: Colors.black,
           primary: Colors.blue,
-          onPrimary: Colors.white
-        )
+          onPrimary: Colors.white,
+        ),
       ),
 
       home: BlocBuilder<AuthenticationBloc, AuthenticationState>(
         builder: ((context, state) {
           if (state.status == AuthenticationStatus.authenticated) {
-            return HomeScreen();
-
+            return BlocProvider(
+              create: (context) => SignInBloc(
+                context.read<AuthenticationBloc>().userRepository
+              ),
+              child: HomeScreen(),
+            );
           } else {
             return WelcomeScreen();
           }
-        })
+        }),
       ),
     );
   }
