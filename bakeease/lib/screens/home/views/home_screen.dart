a91a25ecaca14d9917190a69b0781fd82f60efa0
Prasad Manamperi <prasadmanamperi130@gmail.com>
2026-02-04 03:22:14 +0530

    update: create home screen

--- bakeease/lib/screens/home/views/home_screen.dart
+++ bakeease/lib/screens/home/views/home_screen.dart
@@ -1,4 +1,7 @@
+import 'package:bakeease/screens/auth/blocs/sign_in_bloc/sign_in_bloc.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -6,7 +9,99 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.yellow,
+      backgroundColor: Theme.of(context).colorScheme.surface,
+
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.surface,
+        title: Row(
+          children: [
+            Image.asset('assets/logo.png', scale: 9),
+            const SizedBox(width: 8),
+            Text(
+              "BakeEase",
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+            ),
+          ],
+        ),
+
+        actions: [
+          IconButton(onPressed: () {}, icon: Icon(CupertinoIcons.cart)),
+          IconButton(
+            onPressed: () {
+              context.read<SignInBloc>().add(SignOutRequired());
+            },
+            icon: Icon(CupertinoIcons.arrow_right_to_line),
+          ),
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+
+        child: GridView.builder(
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            crossAxisSpacing: 16,
+            mainAxisSpacing: 16,
+            childAspectRatio: 9/16
+          ),
+          itemCount: 8,
+          itemBuilder: (context, int i){
+            return Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(20.0),
+
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.shade300,
+                    offset: Offset(3, 3),
+                    blurRadius: 5
+                  ),
+                ]
+              ),
+              child: Column(
+                children: [
+                  Container(
+                    width: double.infinity,
+                    height: 200,
+                    decoration: BoxDecoration(
+                      // color: Colors.amberAccent,                   
+                    ),
+                    child: Image.asset(
+                      'assets/Leonardo.jpg',
+                      fit: BoxFit.cover,
+                      
+                    ),
+                  ),
+
+                  Row(
+                    children: [
+                      Container(
+                        margin: EdgeInsets.only(right: 8.0, top: 5.0),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10.0),
+                          color: Colors.red.shade400,
+                      
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 4),
+                          child: Text(
+                            "NON-VEG",
+                            style: TextStyle(
+                              color: Colors.white,
+                          
+                            ),
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ],
+              ),
+            );
+          },
+        ),
+      ),
     );
   }
 }
