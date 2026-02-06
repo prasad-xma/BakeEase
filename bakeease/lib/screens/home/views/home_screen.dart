dd2acc563c801462961a31164594594065ea5e38
Prasad Manamperi <prasadmanamperi130@gmail.com>
2026-02-07 00:40:33 +0530

    update: add bloc provider and load Get Bake bloc

--- bakeease/lib/screens/home/views/home_screen.dart
+++ bakeease/lib/screens/home/views/home_screen.dart
@@ -1,11 +1,26 @@
 import 'package:bakeease/screens/auth/blocs/sign_in_bloc/sign_in_bloc.dart';
+import 'package:bakeease/screens/home/blocs/get_bake_bloc/get_bake_bloc.dart';
 import 'package:bakeease/screens/home/views/details_screen.dart';
+import 'package:bake_repository/bake_repository.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (context) =>
+          GetBakeBloc(context.read<BakeRepo>())..add(GetBakes()),
+      child: const HomeView(),
+    );
+  }
+}
+
+class HomeView extends StatelessWidget {
+  const HomeView({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -35,166 +50,262 @@ class HomeScreen extends StatelessWidget {
           ),
         ],
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-
-        child: GridView.builder(
-          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2,
-            crossAxisSpacing: 16,
-            mainAxisSpacing: 16,
-            childAspectRatio: 9/16
-          ),
-          itemCount: 8,
-          itemBuilder: (context, int i){
-            return Material(
-              elevation: 3,
-              color: Colors.white,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(20),
-              ),
-              child: InkWell(
-                borderRadius: BorderRadius.circular(20),
-                onTap: () {
-                  Navigator.push(
-                    context, 
-                    MaterialPageRoute<void>(
-                      builder: (BuildContext context) => const DetailsScreen(),
-                    ),
-                  );
+      body: BlocBuilder<GetBakeBloc, GetBakeState>(
+        builder: (context, state) {
+          if (state is GetBakeLoading) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (state is GetBakeFailure) {
+            return const Center(child: Text('Failed to load bakes'));
+          } else if (state is GetBakeSuccess) {
+            return Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: GridView.builder(
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  crossAxisSpacing: 16,
+                  mainAxisSpacing: 16,
+                  childAspectRatio: 9 / 16,
+                ),
+                itemCount: state.bakes.length,
+                itemBuilder: (context, index) {
+                  final bake = state.bakes[index];
+                  return BakeCard(bake: bake);
                 },
-                child: Column(
-                  children: [
-                    Container(
-                      width: double.infinity,
-                      height: 170,
+              ),
+            );
+          }
+          return const SizedBox.shrink();
+        },
+      ),
+    );
+  }
+}
+
+class BakeCard extends StatelessWidget {
+  final Bake bake;
+
+  const BakeCard({super.key, required this.bake});
+
+  @override
+  Widget build(BuildContext context) {
+    final discountedPrice = bake.price - (bake.price * bake.discount ~/ 100);
+
+    return Material(
+      elevation: 3,
+      color: Colors.white,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+      child: InkWell(
+        borderRadius: BorderRadius.circular(20),
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute<void>(
+              builder: (BuildContext context) => DetailsScreen(bake: bake),
+            ),
+          );
+        },
+        child: Column(
+          children: [
+            Container(
+              width: double.infinity,
+              height: 170,
+              decoration: BoxDecoration(
+                borderRadius: const BorderRadius.vertical(
+                  top: Radius.circular(20),
+                ),
+                color: Colors.grey[200],
+              ),
+              child: bake.picture.isNotEmpty
+                  ? Image.asset(
+                      'assets/${bake.picture}',
+                      fit: BoxFit.cover,
+                      errorBuilder: (context, error, stackTrace) {
+                        return Container(
+                          decoration: BoxDecoration(
+                            gradient: LinearGradient(
+                              colors: [
+                                Colors.orange.shade100,
+                                Colors.orange.shade200,
+                              ],
+                              begin: Alignment.topLeft,
+                              end: Alignment.bottomRight,
+                            ),
+                          ),
+                          child: const Icon(
+                            Icons.restaurant,
+                            size: 50,
+                            color: Colors.white,
+                          ),
+                        );
+                      },
+                    )
+                  : Container(
                       decoration: BoxDecoration(
-                        // color: Colors.amberAccent,                   
+                        gradient: LinearGradient(
+                          colors: [
+                            Colors.orange.shade100,
+                            Colors.orange.shade200,
+                          ],
+                          begin: Alignment.topLeft,
+                          end: Alignment.bottomRight,
+                        ),
                       ),
-                      child: Image.asset(
-                        'assets/1.png',
-                        fit: BoxFit.cover,
-                        
+                      child: const Icon(
+                        Icons.restaurant,
+                        size: 50,
+                        color: Colors.white,
                       ),
                     ),
-                                
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      
-                      children: [
-                        // non-veg container
-                        Container(
-                          margin: EdgeInsets.only(right: 8.0, top: 2.0),
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(10.0),
-                            color: Colors.red.shade400,
-                        
-                          ),
-                          child: Padding(
-                            padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
-                            child: Text(
-                              "NON-VEG",
-                              style: TextStyle(
-                                color: Colors.white,
-                              ),
-                            ),
-                          ),
+            ),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                  margin: const EdgeInsets.only(right: 8.0, top: 2.0),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(12.0),
+                    color: bake.category.toLowerCase() == 'non-veg'
+                        ? Colors.red.shade500
+                        : Colors.green.shade500,
+                    boxShadow: [
+                      BoxShadow(
+                        color: bake.category.toLowerCase() == 'non-veg'
+                            ? Colors.red.shade200
+                            : Colors.green.shade200,
+                        blurRadius: 4,
+                        offset: const Offset(0, 2),
+                      ),
+                    ],
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 10,
+                      vertical: 5,
+                    ),
+                    child: Text(
+                      bake.category.toUpperCase(),
+                      style: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 10,
+                        fontWeight: FontWeight.bold,
+                        letterSpacing: 0.5,
+                      ),
+                    ),
+                  ),
+                ),
+                if (bake.discount > 0)
+                  Container(
+                    margin: const EdgeInsets.only(right: 8.0, top: 2.0),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(12.0),
+                      gradient: LinearGradient(
+                        colors: [Colors.amber.shade400, Colors.orange.shade400],
+                        begin: Alignment.topLeft,
+                        end: Alignment.bottomRight,
+                      ),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.amber.shade200,
+                          blurRadius: 4,
+                          offset: const Offset(0, 2),
                         ),
-                                
-                        // balance container
-                        Container(
-                          margin: EdgeInsets.only(right: 8.0, top: 2.0),
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(10.0),
-                            color: Colors.green.withAlpha(65),
-                        
-                          ),
-                          child: Padding(
-                            padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
-                            child: Text(
-                              "BALANCE",
-                              style: TextStyle(
-                                color: Colors.green,
-                                fontWeight: FontWeight.bold
-                              ),
-                            ),
-                          ),
-                        )
                       ],
                     ),
-                    
-                    // item name
-                    Center(
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 2),
-                        child: Text(
-                          'Cheese Pizza',
-                          style: TextStyle(
-                            fontSize: 20,
-                            fontWeight: FontWeight.bold
-                          ),
-                        ),
-                      ),
-                    ),
-                                
-                  // item description
-                  Center(
-                    
                     child: Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 8),
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 10,
+                        vertical: 5,
+                      ),
                       child: Text(
-                        'Crafting joy: your pizza, your rules, best taste!',
-                        style: TextStyle(
-                          color: Colors.grey.shade600,
-                          fontWeight: FontWeight.w600,
-                          fontSize: 12,
+                        "${bake.discount}% OFF",
+                        style: const TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 10,
+                          letterSpacing: 0.5,
                         ),
                       ),
                     ),
                   ),
+              ],
+            ),
+
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 2),
+              child: Text(
+                bake.name,
+                style: const TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                ),
+                maxLines: 1,
+                overflow: TextOverflow.ellipsis,
+                textAlign: TextAlign.center,
+              ),
+            ),
+
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8),
+              child: Text(
+                bake.description,
+                style: TextStyle(
+                  color: Colors.grey.shade600,
+                  fontWeight: FontWeight.w600,
+                  fontSize: 12,
+                ),
+                maxLines: 2,
+                overflow: TextOverflow.ellipsis,
+                textAlign: TextAlign.center,
+              ),
+            ),
+
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 5, vertical: 4.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
                   Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 5, vertical: 4.0),
+                    padding: const EdgeInsets.only(left: 4),
                     child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
-                        Padding(
-                          padding: const EdgeInsets.only(left: 4),
-                          child: Row(
-                            children: [
-                              Text(
-                                '\$12.00',
-                                style: TextStyle(
-                                  color: Theme.of(context).colorScheme.primary,
-                                  fontWeight: FontWeight.w800,
-                                  fontSize: 18.0
-                                ),
-                              ),
-                              const SizedBox(width: 6,),
-                              Text(
-                                '\$15.00',
-                                style: TextStyle(
-                                  color: Colors.grey.shade600,
-                                  fontWeight: FontWeight.w500,
-                                  fontSize: 14.0,
-                                  decoration: TextDecoration.lineThrough, 
-                                ),
-                              )
-                            ],
+                        Text(
+                          '\$${(discountedPrice / 100).toStringAsFixed(2)}',
+                          style: TextStyle(
+                            color: Theme.of(context).colorScheme.primary,
+                            fontWeight: FontWeight.w800,
+                            fontSize: 16.0,
                           ),
                         ),
-                        IconButton(
-                          onPressed: () {},
-                          icon: Icon(CupertinoIcons.add_circled_solid),
-                        )
+                        if (bake.discount > 0) ...[
+                          const SizedBox(width: 6),
+                          Text(
+                            '\$${(bake.price / 100).toStringAsFixed(2)}',
+                            style: TextStyle(
+                              color: Colors.grey.shade600,
+                              fontWeight: FontWeight.w500,
+                              fontSize: 12.0,
+                              decoration: TextDecoration.lineThrough,
+                            ),
+                          ),
+                        ],
                       ],
                     ),
-                  )
-                  ],
-                ),
+                  ),
+                  IconButton(
+                    onPressed: () {
+                      // TODO: Add to cart functionality
+                    },
+                    icon: Icon(
+                      CupertinoIcons.add_circled_solid,
+                      color: Theme.of(context).colorScheme.primary,
+                    ),
+                  ),
+                ],
               ),
-            );
-          },
+            ),
+          ],
         ),
       ),
     );
